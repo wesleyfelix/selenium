991e340f6ceb67aa0b4936a34595d118034a07a8
wesleyfelix <wesley.felixdelima96@gmail.com>
2021-11-12 16:38:16 -0300

    remocao bibliotecas sem uso

--- src/test/java/br/com/alura/leilao/login/LoginTest.java
+++ src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -2,13 +2,9 @@ package br.com.alura.leilao.login;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class LoginTest {
 
